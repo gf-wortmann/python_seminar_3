3ecb598e1fc188f7e26c40151d6521ab8dd00267
George Wortmann <gf.wortmann@gmail.com>
2023-03-27 02:31:06 +0300

    task 20 corrected 1

--- program.py
+++ program.py
@@ -1,42 +1,45 @@
-# Задача 16:
-# Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
-# Пользователь вводит натуральное число N – количество элементов в массиве и число, которое необходимо проверить - X.
-# Заполните массив случайными натуральными числами от 1 до N/2.
-# Выведите, сколько раз X встречается в массиве.
+# Задача 20:
+# В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+# В случае с английским алфавитом очки распределяются так:
+# A, E, I, O, U, L, N, S, T, R – 1 очко;
+# D, G – 2 очка;
+# B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка;
+# K – 5 очков;
+# J, X – 8 очков;
+# Q, Z – 10 очков.
 
-# Ввод: 5
-# Ввод: 1
+# А русские буквы оцениваются так:
+# А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+# Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков.
 
-# 1 2 1 2 2
-# Вывод: 2
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
 
-from random import randrange
-from input_check import IntCheckedInputLtd
+# Ввод: ноутбук
+# Вывод: 12
 
-arraySizeLimit = 101 # limited just for simplicity
-arraySize = IntCheckedInputLtd(
-    f'Enter the size of the array, not more than {arraySizeLimit-1}',
-    arraySizeLimit)
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'XJ', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = input('Enter the word: ').upper()
+#print (word)
+wordPrice = 0
+lettercount = 0
 
-RandomArray = [randrange ( 1, arraySize // 2 ) for i in range(arraySize)]
-print(RandomArray)  # it is interesting to see what the progam has added in into the array
 
-arrayMenber = IntCheckedInputLtd(
-    f'Enter the number to be found in the array, not more than {arraySizeLimit // 2}',
-    arraySizeLimit // 2 + 1)
+for letter in word:
+    for letterPrice in points_en:
+        if letter in points_en[letterPrice]:
+            wordPrice += letterPrice
+            lettercount +=1
+    if lettercount == 0:
+        for letterPrice in points_ru:
+            if letter in points_ru[letterPrice]:
+                wordPrice += letterPrice
 
-print(f'Count of {arrayMenber} in the array founded bu function "array.count" is:  {RandomArray.count(arrayMenber)}')
-
-memberCount = 0
-for i in RandomArray:
-    if i == arrayMenber:
-        memberCount += 1
-
-print(f'Count of {arrayMenber} in the array founded by iteration is: {memberCount}')
-
-memberCount = 0
-for i in range(arraySize):
-    if RandomArray [i] == arrayMenber:
-        memberCount += 1
-
-print(f'Count of {arrayMenber} in the array founded by index cycle is: {memberCount}')
+print(wordPrice)
