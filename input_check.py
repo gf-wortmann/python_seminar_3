dbfee43236c9a0b728dde80a55e6e4ab9c5990ae
George Wortmann <gf.wortmann@gmail.com>
2023-03-27 03:24:05 +0300

    task 16 corrected 6

--- input_check.py
+++ input_check.py
@@ -1,1 +1,29 @@
-/home/george/Documents/Learning/Python/python_course_1/seminar_2/input_check.py
+
+
+from curses.ascii import isdigit
+
+
+def IntCheckedInput (message:str) :
+    while True:
+        res = input(f'{message}: ')
+        if res.isdigit():
+            return int(res)
+        print('Enter digits only, please!')
+
+def IntCheckedInputLtd (message:str, max:int) :
+    while True:
+        res = input(f'{message}: ')
+        if res.isdigit():
+            if int(res)<int(max):
+                return int(res)
+            else:
+                print(f'number must be less than {max}')
+        else:
+            print('Enter digits only, please!')        
+
+def OneOrZero(messge:str):
+    while True:
+        res = input(f'{messge}: ')
+        if res == '1' or res == '0':
+            return res
+        print('Enter "1" or "0", please')
